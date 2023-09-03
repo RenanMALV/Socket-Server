4475a1cffe071a4aa364b49ed258addf6f07fead
Renan Mendanha <renanma@dcc.ufrj.br>
2023-09-03 05:15:14 -0300

    Writing single client server code

---
+++ server.py
@@ -0,0 +1,84 @@
+from socket import *
+
+def sendAvailableWords():
+    words = bytearray()
+
+    for word in list(portToEnglish.keys()):
+        words.extend((word + ' ').encode())
+
+    connectionSocket.send(words)
+
+def sendInvalidMessage():
+    connectionSocket.send(convertToBytes('INVALID'))
+
+def convertToBytes(string):
+    return bytearray(string, 'utf-8')
+
+def convertToString(byte):
+    return str(message, encoding = 'utf-8')
+
+portToEnglish = {
+    'enlace': 'link',
+    'roteador': 'router',
+    'pacote': 'packet',
+    'rede': 'network',
+    'protocolo': 'protocol',
+    'hospedeiro': 'host',
+    'mensagem': 'message',
+    'propagação': 'propagation',
+    'atraso': 'delay',
+    'transmissão': 'transmission',
+}
+
+portToFrench = {
+    'enlace': 'lien',
+    'roteador': 'routeur',
+    'pacote': 'paquet',
+    'rede': 'réseau',
+    'protocolo': 'protocole',
+    'hospedeiro': 'hôte',
+    'mensagem': 'message',
+    'propagação': 'propagation',
+    'atraso': 'retard',
+    'transmissão': 'transmission',
+}
+
+serverPort = 80
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+
+serverSocket.listen(1)
+print('Servidor está pronto para receber...')
+
+connectionSocket, address = serverSocket.accept()
+print ('Conectado com: ', address)
+
+choice, clientAddress = connectionSocket.recvfrom(2048)
+
+if int(choice) == 1:
+    dictionary = portToEnglish    
+elif int(choice) == 2:
+    dictionary = portToFrench
+else:
+    sendInvalidMessage()
+    exit()
+
+# envia as palavras disponiveis no dicionario
+sendAvailableWords()
+
+while 1:
+    message, clientAddress = connectionSocket.recvfrom(2048)
+    key = convertToString(message)
+
+    # se receber SAIR, encerra a conexao
+    if not message:
+        break
+
+    # se o cliente quiser uma chave que nao existe no dicionario, envia uma resposta de invalidez
+    if key not in dictionary:
+        sendInvalidMessage()
+        break
+
+    connectionSocket.send(convertToBytes(dictionary[key]))
+
+connectionSocket.close()
---
+++ webServer.py
@@ -0,0 +1,118 @@
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+import os # get file details
+import time # to send time in response header lines
+import signal # for manually closing the socket 
+
+#Prepare a sever socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverAddr = 'localhost'
+serverPort = 80
+serverSocket.bind((serverAddr, serverPort))
+serverSocket.listen(1)
+serverSocket.settimeout(5.0)
+print('Servidor inicializado em ', serverSocket.getsockname())
+
+def closeServer(sigNumber, _):
+    print("Encerrando servidor...", "Sinal:", signal.Signals(sigNumber).name)
+    serverSocket.close()
+    sys.exit()#Terminate the program after sending the corresponding data
+
+signal.signal(signal.SIGINT, closeServer)
+
+while True:
+    try:
+        #Establish the connection
+        #print("blocking")
+        connectionSocket, clientAddr = serverSocket.accept()
+        print ('Novo cliente conectado:', clientAddr)
+    except timeout as timeOut_e:
+        #print(timeOut_e)
+        continue
+
+    try:
+        # receive a request
+        messageBytes, msgAddr = connectionSocket.recvfrom(2048)
+
+        if not messageBytes:
+            connectionSocket.close()
+            continue
+
+        #print(messageBytes.decode())
+        message = messageBytes.decode()
+        filename = message.split()[1]
+        print("Requisitou:", filename)
+        
+        # if in initial page, redirect to index.html
+        if(filename == "/"):
+            print("Redirecionando para o index")
+            filename = "/index.html"
+        
+        f = open(filename[1:])
+        outputdata = f.read()
+        f.close()
+
+        #Send one HTTP header line into socket
+        header = "HTTP/1.1 200 OK\r\n"
+        # time
+        headerLines ="Date: " + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) + "\r\n"
+        # server
+        headerLines += "Server: Python_Socket\r\n" 
+        # content length
+        headerLines += "Content-Length: " + str(os.path.getsize(filename[1:])) + "\r\n"
+        # content type
+        fileType = filename.split('.')[-1]
+        if(fileType == "html"):
+            headerLines += "Content-Type: text/html\r\n"
+        else:
+            headerLines += "Content-Type: " + fileType + "\r\n"
+        # connection close (connection will be closed after the response)
+        headerLines += 'Connection: close\r\n\n'
+        responseHeader = header + headerLines
+        print(responseHeader)
+
+        outputdata = responseHeader + outputdata
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError as e:
+        print("Capturada uma exceção:\n     ", e)
+        # error 404 Not Found
+        if(e.errno == 2):
+            #try to send one HTTP 404 header line into socket
+            try:
+                header = "HTTP/1.1 404 Not Found\r\n"
+                # time
+                headerLines ="Date: " + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()) + "\r\n"
+                # server
+                headerLines += "Server: Python_Socket\r\n"
+                # content type
+                headerLines += "Content-Type: text/html\r\n"
+                # connection close (connection will be closed after the response)
+                headerLines += 'Connection: close\r\n\n'
+                
+                responseHeader = header + headerLines
+                print(responseHeader)
+                outputdata = responseHeader + (
+                    "<!doctype html><html> <head></head>" +
+                        "<body>" +
+                            "<center><h1>Error 404: Not found</h1></center>" +
+                        "</body>" + 
+                    "</html>")
+                #print(outputdata)
+                #Send response message for file not found
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send("\r\n".encode())
+            except ConnectionError as conn_e:
+                print("Capturado um erro de conexão:", conn_e)
+        #Close client socket
+        connectionSocket.close()
+        continue
+
+#closeServer()
+# should not reach this section of code...
+# all exceptions should be treated and connections closed
